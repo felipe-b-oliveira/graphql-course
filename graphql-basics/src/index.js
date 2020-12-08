a06e251a5122c0d1aead9866dba6748ab087fb8a
Felipe Oliveira <o.felipe93@gmail.com>
2020-12-08 10:22:38 -0300

    Trabalhando com arrays de tipos customizados

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -2,14 +2,59 @@ import { GraphQLServer } from 'graphql-yoga';
 
 // Scalar types - String, Boolean, Int, Float, ID
 
+// Demo user data
+const users = [{
+  id: '1',
+  name: 'Felipe',
+  email: 'felipe@exemplo.com',
+  age: 27
+}, {
+  id: '2',
+  name: 'Sarah',
+  email: 'sarah@exemplo.com',
+  age: 32
+}, {
+  id: '3',
+  name: 'Mike',
+  email: 'mike@exemplo.com',
+  age: 32
+}, {
+  id: '4',
+  name: 'Andrew',
+  email: 'andrew@exemplo.com',
+  age: 23
+}]
+
+const posts = [{
+  id: '101',
+  title: 'Happy New Year',
+  body: 'A great new year awaits for us',
+  published: true
+}, {
+  id: '102',
+  title: 'Christimas comes',
+  body: 'I love christmas lights',
+  published: true
+}, {
+  id: '103',
+  title: 'The summer has come',
+  body: 'I love swimming in a hot summer day',
+  published: true
+}, {
+  id: '104',
+  title: 'I loves holidays',
+  body: 'Break up from the rotine',
+  published: false
+}
+]
+
 // Type Definitions (schema)
 const typeDefs = `
   type Query {
-    fetchUser: User!
-    fetchPost: Post!
-    greeting(name: String, position: String): String!
-    add(numbers: [Float!]!): Float!
-    grades: [Int!]!
+    users(query: String): [User!]!
+    posts(query: String): [Post!]!
+    me: User!
+    post: Post!
   }
 
   type User {
@@ -30,7 +75,28 @@ const typeDefs = `
 // Resolvers 
 const resolvers = {
   Query: {
-    fetchUser() {
+    users(parent, args, ctx, info) {
+      if(!args.query) {
+        return users
+      }
+
+      return users.filter((user) => {
+        return user.name.toLowerCase().includes(args.query.toLowerCase())
+      })
+    },
+    posts(parent, args, ctx, info) {
+      if(!args.query) {
+        return posts
+      }
+
+      return posts.filter((post) => {
+        const isTitle = post.title.toLowerCase().includes(args.query.toLowerCase())
+        const isBody = post.body.toLowerCase().includes(args.query.toLowerCase())
+
+        return isTitle || isBody;
+      })
+    },
+    me() {
       return {
         id: '123098',
         name: 'Mike',
@@ -38,32 +104,13 @@ const resolvers = {
         age: 28
       }
     },
-    fetchPost() {
+    post() {
       return {
         id: 'p12309',
         title: 'Happy New Year',
         body: 'I love December feels',
         published: true
       }
-    },
-    greeting(parent, args, ctx, info) {
-      if(args.name && args.position) {
-        return `Hello ${args.name}! You are my favorite ${args.position}`
-      } else {
-        return 'Hello!'
-      }
-    },
-    add(parent, args, ctx, info) {
-      if(args.numbers.length === 0) {
-        return 0
-      }
-
-      return args.numbers.reduce((acc, currValue) => {
-        return acc + currValue
-      })
-    },
-    grades(parent, args, ctx, info) {
-      return [99, 85, 75]
     }
   }
 }
